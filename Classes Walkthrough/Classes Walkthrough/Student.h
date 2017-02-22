dae9348073bb30acdfb5a727078bef3c946d41f6
ncc <ncc>
2017-02-22 09:53:18 -0800

    class files added

---
+++ Classes Walkthrough/Classes Walkthrough/Student.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <string>
+
+class Student
+{
+private:
+	std::string _name;	//Go to namespace std, pull out string class, name it _name (can use "using namespace std;" at top also)
+public:
+	Student(std::string name);
+	~Student();
+
+	std::string GetName();
+};
+
