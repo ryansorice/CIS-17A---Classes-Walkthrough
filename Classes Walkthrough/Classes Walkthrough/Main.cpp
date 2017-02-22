259de4d9cb3d4dca341cf2c9fc77926dbc9b8418
ncc <ncc>
2017-02-22 09:48:33 -0800

    Teacher class Finished

--- Classes Walkthrough/Classes Walkthrough/Main.cpp
+++ Classes Walkthrough/Classes Walkthrough/Main.cpp
@@ -1,9 +1,16 @@
 #include "Student.h"
+#include "Teacher.h"
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
+	Teacher james = Teacher("james");
+	Teacher bob = Teacher("bob");
+	cout << james.GetName() << endl;
+	cout << bob.GetName() << endl;
+
+	getchar();
 	return 0;
 }
